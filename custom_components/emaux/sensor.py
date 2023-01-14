778c7c8fd3cf605f159d803ad2587e89f70a2740
Dan Turner <dan.p.turner@gmail.com>
2023-01-14 14:14:44 +1100

    feat: add speed and power sensors

--- custom_components/emaux/sensor.py
+++ custom_components/emaux/sensor.py
@@ -6,10 +6,11 @@ from homeassistant.components.sensor import (
     SensorEntity,
     SensorStateClass,
 )
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import POWER_WATT, REVOLUTIONS_PER_MINUTE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from emaux_client import pump
 
 
 def setup_platform(
@@ -19,20 +20,38 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+    add_entities([PoolPumpSpeedSensor(), PoolPumpPowerSensor()])
 
 
-class ExampleSensor(SensorEntity):
-    """Representation of a Sensor."""
+class PoolPumpSpeedSensor(SensorEntity):
+    """Pool pump speed sensor."""
 
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _pump = pump.Pump("http://192.168.1.94")
+
+    _attr_name = "Pool Pump Speed"
+    _attr_native_unit_of_measurement = REVOLUTIONS_PER_MINUTE
+    _attr_device_class = SensorDeviceClass.SPEED
     _attr_state_class = SensorStateClass.MEASUREMENT
 
     def update(self) -> None:
         """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_native_value = 23
+        data = self._pump.get_data()
+        self._attr_native_value = data.speed
+
+
+class PoolPumpPowerSensor(SensorEntity):
+    """Pool pump power sensor."""
+
+    _pump = pump.Pump("http://192.168.1.94")
+
+    _attr_name = "Pool Pump Power"
+    _attr_native_unit_of_measurement = POWER_WATT
+    _attr_device_class = SensorDeviceClass.POWER
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+        """
+        data = self._pump.get_data()
+        self._attr_native_value = data.power
